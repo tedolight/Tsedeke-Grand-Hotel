b4ebee0278caa9c49329e91fed9559564fd7776c
tedolight <tedit3833@gmail.com>
2026-09-14 02:29:52 -0700

    chore: add vercel SPA routing configs, dynamic VITE_API_URL, and layout fixes

--- admin/src/services/api/api.js
+++ admin/src/services/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api` 
+    : '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
